2d5c956a04bcf2e601b9369b477aa5ea5cef1476
Aditya Gupta <adityasushilgupta04@gmail.com>
2026-07-31 20:20:12 +0530

    Remove All Adjacent Duplicates in String II

---
+++ Remove_All_Adjacent_Duplicates_in_String_II.java
@@ -0,0 +1,89 @@
+/*
+
+You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+Example 2:
+
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation: 
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+Example 3:
+
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+2 <= k <= 104
+s only contains lowercase English letters.
+
+*/
+import java.util.Stack;
+
+class Remove_All_Adjacent_Duplicates_in_String_II {
+
+    class Pair {
+        char ch;
+        int count;
+
+        Pair(char ch, int count) {
+            this.ch = ch;
+            this.count = count;
+        }
+    }
+
+    public String removeDuplicates(String s, int k) {
+
+        Stack<Pair> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            if (stack.isEmpty()) {
+                stack.push(new Pair(c, 1));
+            } 
+            else if (stack.peek().ch == c) {
+
+                stack.peek().count++;
+
+                if (stack.peek().count == k) {
+                    stack.pop();
+                }
+
+            } 
+            else {
+                stack.push(new Pair(c, 1));
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+
+            Pair p = stack.pop();
+
+            for (int i = 0; i < p.count; i++) {
+                ans.append(p.ch);
+            }
+        }
+
+        return ans.reverse().toString();
+    }
+}
