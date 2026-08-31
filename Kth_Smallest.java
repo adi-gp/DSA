355ff577a2922376b8577d6deba6958104091407
Aditya Gupta <adityasushilgupta04@gmail.com>
2026-08-31 23:59:00 +0530

    Add Kth Smallest solution

---
+++ Kth_Smallest.java
@@ -0,0 +1,35 @@
+/*
+
+Given an integer array arr[] and an integer k, find and return the kth smallest element in the given array.
+Note: The kth smallest element is determined based on the sorted order of the array.
+
+Examples :
+
+Input: arr[] = [10, 5, 4, 3, 48, 6, 2, 33, 53, 10], k = 4
+Output: 5
+Explanation: 4th smallest element in the given array is 5.
+Input: arr[] = [7, 10, 4, 3, 20, 15], k = 3
+Output: 7
+Explanation: 3rd smallest element in the given array is 7.
+Constraints:
+1 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 105
+1 ≤ k ≤  arr.size() 
+
+*/
+import java.util.*;
+class Kth_Smallest {
+    public int kthSmallest(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0 ; i<k; i++){
+            pq.add(arr[i]);
+        }
+        for(int i = k ; i < arr.length ; i++){
+            if(pq.peek()>arr[i]){
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        return pq.peek();
+    }
+}
