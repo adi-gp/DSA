405910427f4a1c93a1f574fa27c668d7a777f044
Aditya Gupta <adityasushilgupta04@gmail.com>
2026-09-01 23:59:00 +0530

    Add Kth Largest Element solution

---
+++ Kth_Largest_Element_in_an_Array.java
@@ -0,0 +1,43 @@
+/*
+
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 105
+-104 <= nums[i] <= 104
+
+*/
+import java.util.*;
+class Kth_Largest_Element_in_an_Array {
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue <Integer> heap = new PriorityQueue<>();
+        for(int i = 0 ; i<k ; i++){
+            heap.add(nums[i]);
+        }
+        for(int i = k ; i<n ; i++){
+            if(heap.peek()< nums[i]){
+                heap.poll();
+                heap.add(nums[i]);
+            }
+        }
+        return heap.peek();
+    }
+}
